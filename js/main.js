1440769b6bc702989c9a399975125aeb0a879dc9
Matteo Magliano <matteo.magliano01@gmail.com>
2020-01-17 17:04:40 +0100

    next completo

--- js/main.js
+++ js/main.js
@@ -27,14 +27,35 @@ $(document).ready(
 // -------- Function --------
 
 function clickNext() {
+
+// ------Variabili image
   var imageActive = $('img.active');
   var nextImage = imageActive.next();
-  if (imageActive.hasClass('last') == true) {
+
+// ------Variabili circle
+
+var circleActive = $('.active');
+var nextCircle = circleActive.next()
+
+  if (imageActive.hasClass('last') == true && circleActive.hasClass('last') == true) {
+
+//      IMAGE
     imageActive.removeClass('active');
     $('img.first').addClass('active');
+
+//      CIRCLE
+    circleActive.removeClass('active')
+    $('i.first').addClass('active');
+
   } else {
+
+    //      IMAGE
     imageActive.removeClass('active');
     nextImage.addClass('active');
+
+//      CIRCLE
+    circleActive.removeClass('active');
+    nextCircle.addClass('active');
   }
 
 
