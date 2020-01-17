dd4b69118ce286efff42e3286d48ffa6d311ffa1
Matteo Magliano <matteo.magliano01@gmail.com>
2020-01-17 16:38:53 +0100

    slide da img1 a img4

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
-$(document).ready(function() {
-  alert('Hello!')
-})
+$(document).ready(
+  function() {
+
+//---- Click Next -----------
+    $('.next').click(
+      function() {
+        clickNext();
+      }
+    );
+
+//---- Click Prev -----------
+    $('.prev').click(
+      function() {
+        clickPrev();
+      }
+    );
+
+});
+
+
+
+
+
+
+
+
+// -------- Function --------
+
+function clickNext() {
+  var imageActive = $('img.active');
+  var nextImage = imageActive.next();
+  imageActive.removeClass('active');
+  nextImage.addClass('active');
+
+}
